6be48f8e0f6a1e0734a9e672f0d15d32d3a7c73f
axdjuraev <axdjuraev@gmail.com>
2024-02-03 15:07:43 +0500

    feat: EventLoopQueue

--- grateful_logging/handler/event_loop_queue.py
+++ grateful_logging/handler/event_loop_queue.py
@@ -4,8 +4,8 @@ from .queue import QueueHandler
 
 class EventLoopQueue(QueueHandler):
     def __init__(self, *args, loop: "asyncio.AbstractEventLoop | None" = None, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
         self._loop = loop or asyncio.new_event_loop()
+        super().__init__(*args, **kwargs)
 
     def _shutup(self):
         self._loop.close()
